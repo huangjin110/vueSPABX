4ba65f679cfa5f11b32ef2fa3fecb19faa1eaae6
unknown <huangjin1@zlketang.com>
2020-04-19 22:59:58 +0800

    路由懒加载

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import router from './router/index'
+
 import App from './App.vue'
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,11 @@ import Vue from 'vue'
 import vueRouter from 'vue-router'
 Vue.use(vueRouter)
 
-import center from '@/views/center.vue'
-import myCourse from '@/views/myCourse.vue'
-import choseCourse from '@/views/choseCourse.vue'
-
 const routes = [
-    { path:'/',component: center},
-    { path: '/center', component: center },
-    { path: '/myCourse', component: myCourse },
-    { path: '/choseCourse', component: choseCourse },
+    { path:'/',component: ()=>import('@/views/center.vue')},
+    { path: '/center', component: ()=>import('@/views/center.vue') },
+    { path: '/myCourse', component: ()=>import('@/views/myCourse.vue') },
+    { path: '/choseCourse', component: ()=>import('@/views/choseCourse.vue')},
 ]
 
 let router = new vueRouter({
